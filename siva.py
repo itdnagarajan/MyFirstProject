d7347c9fdc02f2342dab7cd2a0f8563f689cdab5
unknown <itdnagarajan@gmail.com>
2020-01-26 19:43:26 +0530

    initial commit

---
+++ siva.py
@@ -0,0 +1,14 @@
+num1 = 20
+num2 = 0
+
+if num1:
+    print("num1 has some value which is ", num1);
+else:
+    print("num1 hasn't any value");
+
+if num2:
+    print("num2 has some value which is", num2);
+else:
+    print("num2 hasn't any value");
+
+print("exiting...");
---
+++ test2.py
@@ -0,0 +1,5 @@
+print("Welcome to Python if Statement Example.");
+print("You are here at codescracker.com");
+pswd = input("Enter the password:");
+if pswd == "pythonAtCodescracker":
+    print("Access Granted.");
